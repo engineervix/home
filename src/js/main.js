bc930a6c4536b061ea75876834e2bf9ac6384022
Victor Miti <victormiti@umusebo.com>
2025-04-23 23:31:28 +0100

    feat: add Website Carbon Badge
    https://www.websitecarbon.com/badge/

--- src/js/main.js
+++ src/js/main.js
@@ -26,11 +26,17 @@ themeToggleBtn.addEventListener('click', () => {
   const currentTheme = document.body.className.includes('theme-dark') ? 'dark' : 'light'
   const newTheme = currentTheme === 'light' ? 'dark' : 'light'
   setTheme(newTheme)
+
+  // Update badge theme
+  updateCarbonBadgeTheme(newTheme === 'dark')
 })
 
 // Listen for changes in system preferences
 prefersDarkScheme.addEventListener('change', (e) => {
   setTheme(e.matches ? 'dark' : 'light')
+
+  // Update badge theme
+  updateCarbonBadgeTheme(e.matches)
 })
 
 /* █████████████████████████████████ FOOTER █████████████████████████████████ */
@@ -44,4 +50,29 @@ const buildDate = dayjs(__BUILD_DATE__)
 // Calculate the relative time from the build date
 const lastRebuilt = buildDate.fromNow()
 
-footer.innerHTML = `© ${currentYear}, Victor Miti. <span>Last updated ${lastRebuilt}.</span>`
+footer.innerHTML = `<p>© ${currentYear}, Victor Miti. <span>Last updated ${lastRebuilt}.</span></p>`
+
+// Add carbon badge
+const carbonBadge = document.createElement('div')
+carbonBadge.id = 'wcb'
+carbonBadge.classList.add('carbonbadge')
+
+footer.appendChild(carbonBadge)
+
+// Load the carbon badge script
+const carbonScript = document.createElement('script')
+carbonScript.src = 'https://unpkg.com/website-carbon-badges@1.1.3/b.min.js'
+carbonScript.defer = true
+document.body.appendChild(carbonScript)
+
+// Update carbon badge theme
+function updateCarbonBadgeTheme(isDarkTheme) {
+  if (isDarkTheme) {
+    carbonBadge.classList.add('wcb-d')
+  } else {
+    carbonBadge.classList.remove('wcb-d')
+  }
+}
+
+// Apply initial theme
+updateCarbonBadgeTheme(document.body.className.includes('theme-dark'))
